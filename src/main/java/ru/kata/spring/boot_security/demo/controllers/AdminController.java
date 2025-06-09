65b52ad76a91f1855a55ff58537cc4bcdf3d0d1b
Sergei Potapov <Potapov1995@bk.ru>
2025-06-09 17:34:32 +0300

    task 3.1.3 update done

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -9,14 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+
 
 @Controller
 @RequestMapping("/admin")
@@ -34,33 +32,21 @@ public class AdminController {
     @GetMapping
     public String adminPage(Model model) {
         model.addAttribute("users", userService.getAllUsers());
-        return "admin/admin-page";
-    }
-
-    @GetMapping("/add")
-    public String addUserForm(Model model) {
         model.addAttribute("user", new User());
         model.addAttribute("roles", roleService.getRoles());
-        return "admin/user-form";
+        return "admin/admin-page";
     }
 
     @PostMapping("/save")
     public String saveUser(@ModelAttribute("user") User user,
-                           @RequestParam("roles") List<Long> roleIds) {
+                           @RequestParam("roleIds") List<Long> roleIds) {
         userService.addUserWithRoles(user, roleIds);
         return "redirect:/admin";
     }
 
-    @GetMapping("/edit/{id}")
-    public String editUserForm(@PathVariable Long id, Model model) {
-        model.addAttribute("user", userService.getById(id));
-        model.addAttribute("roles", roleService.getRoles());
-        return "admin/user-form";
-    }
-
     @PostMapping("/update")
     public String updateUser(@ModelAttribute("user") User user,
-                             @RequestParam("roles") List<Long> roleIds) {
+                             @RequestParam("roleIds") List<Long> roleIds) {
         userService.updateUserWithRoles(user, roleIds);
         return "redirect:/admin";
     }
